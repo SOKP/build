c6d7cadbb9398c38b4064865d9211fe45664db2b
AudioGod <adi.nth@outlook.com>
2015-05-25 09:47:25 +0530

    SOKP Build

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Copyright (C) 2012-2013, The CyanogenMod Project
 # Copyright (C) 2012-2015, SlimRoms Project
+# Copyright (C) 2015, SOnic Open Kang Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -42,11 +43,11 @@ except ImportError:
 DEBUG = False
 default_manifest = ".repo/manifest.xml"
 
-custom_local_manifest = ".repo/local_manifests/slim_manifest.xml"
-custom_default_revision = "lp5.0"
-custom_dependencies = "slim.dependencies"
-org_manifest = "SlimRoms"  # leave empty if org is provided in manifest
-org_display = "SlimRoms"  # needed for displaying
+custom_local_manifest = ".repo/local_manifests/sokp_manifest.xml"
+custom_default_revision = "sokp-l5.1"
+custom_dependencies = "sokp.dependencies"
+org_manifest = "SOKP"  # leave empty if org is provided in manifest
+org_display = "SOKP"  # needed for displaying
 
 github_auth = None
 
@@ -180,7 +181,7 @@ def add_to_manifest(repos, fallback_branch=None):
         project = ElementTree.Element(
             "project",
             attrib={"path": repo_target,
-                    "remote": "github",
+                    "remote": "sokp",
                     "name": "%s" % repo_name}
         )
 
